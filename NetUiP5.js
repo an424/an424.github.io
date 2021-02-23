3e5d6a9bd1d1c4d21eb6cdc3ce6284c36ed65cd0
Alfie Newman <61122070+an424@users.noreply.github.com>
2021-02-23 12:45:54 +0000

    Update NetUiP5.js

--- NetUiP5.js
+++ NetUiP5.js
@@ -127,7 +127,7 @@ class Circle
     this.w = wdth;
     this.h = hght;
 
-    this.c = color(this.rndColour(), this.rndColour(), this.rndColour(), (this.rndColour() / 5));
+    this.c = color(this.rndColour(), this.rndColour(), this.rndColour(), 125);
   }
   
   display()
@@ -140,7 +140,7 @@ class Circle
 
   changeColour()
   {
-    this.c = color(this.rndColour(), this.rndColour(), this.rndColour(), (this.rndColour() / 5));
+    this.c = color(this.rndColour(), this.rndColour(), this.rndColour(), 125);
   }
 }
 
